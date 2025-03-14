7a67ff1699de84d1c709751303bd1bbd6ae588bc
shivakumar0002 <godipally.shivakumar01@gmail.com>
2025-03-14 17:07:33 -0400

    new updates

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,4 +1,11 @@
+import pytest
 
+
+class NotInCustomError(Exception):
+    def __init__(self, input_, message="Value not in range"):
+        self.input_ = input_
+        self.message = message
+        super().__init__(self.message)
 
 def test_generic():
     a = 2
