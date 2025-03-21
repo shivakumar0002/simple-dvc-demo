3b68354961b654145cab9b6eef38fb09d70169f5
shivakumar0002 <godipally.shivakumar01@gmail.com>
2025-03-21 16:17:56 -0400

    ✅ Fix: Cast model responses to float in tests

--- tests/test_config.py
+++ tests/test_config.py
@@ -57,11 +57,11 @@ TARGET_range = {
 
 def test_form_response_correct_range(data=input_data["correct_range"]):
     res = form_response(data)
-    assert  TARGET_range["min"] <= res <= TARGET_range["max"]
+    assert TARGET_range["min"] <= float(res) <= TARGET_range["max"]
 
 def test_api_response_correct_range(data=input_data["correct_range"]):
     res = api_response(data)
-    assert  TARGET_range["min"] <= res["response"] <= TARGET_range["max"]
+    assert TARGET_range["min"] <= float(res["response"]) <= TARGET_range["max"]
 
 def test_form_response_incorrect_range(data=input_data["incorrect_range"]):
     with pytest.raises(prediction_service.prediction.NotInRange):
